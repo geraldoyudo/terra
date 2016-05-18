8de13f880905ed70ad319347f066fc9c84417034
Gerald Oyudo <gerald.oyudo@isslng.com>
2016-05-18 12:22:43 +0100

    marked production config

--- code/terra-journal/src/main/java/com/isslng/terra/journal/TerraJournalProcessorApplication.java
+++ code/terra-journal/src/main/java/com/isslng/terra/journal/TerraJournalProcessorApplication.java
@@ -2,11 +2,19 @@ package com.isslng.terra.journal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class TerraJournalProcessorApplication {
+public class TerraJournalProcessorApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TerraJournalProcessorApplication.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TerraJournalProcessorApplication.class);
+    }
+
 }
