bdbf34bd415ec1f95cab36b59447ddf249575636
Gerald Oyudo <gerald.oyudo@isslng.com>
2016-10-10 11:10:48 +0100

    updated config

--- code/terra-storage/src/main/java/com/isslng/terra/storage/MultiTenantTestApplication.java
+++ code/terra-storage/src/main/java/com/isslng/terra/storage/MultiTenantTestApplication.java
@@ -2,15 +2,21 @@ package com.isslng.terra.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import com.isslng.terra.storage.util.MultiTenantProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(MultiTenantProperties.class)
-public class MultiTenantTestApplication {
+public class MultiTenantTestApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiTenantTestApplication.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MultiTenantTestApplication.class);
+    }
 }
